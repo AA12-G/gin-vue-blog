f956a0814e47e7ebe96ec4180b32fafdb394ab9e
AA12-G <811959019@qq.com>
2024-09-02 23:44:25 +0800

    gorm配置初始化

---
+++ gin-vue-blog_server/core/gorm.go
@@ -0,0 +1,40 @@
+package core
+
+import (
+	"fmt"
+	"gin-vue-blog_server/global"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+	"time"
+)
+
+func InitGorm() *gorm.DB {
+	if global.Config.Mysql.Host == "" {
+		log.Println("未配置mysql，取消gorm连接")
+		return nil
+	}
+	dsn := global.Config.Mysql.Dsn()
+
+	var mysqlLogger logger.Interface
+	if global.Config.System.Env == "debug" {
+		// 开发环境显示的sql
+		mysqlLogger = logger.Default.LogMode(logger.Info)
+	} else {
+		mysqlLogger = logger.Default.LogMode(logger.Error)
+	}
+	//global.MysqlLog = logger.Default.LogMode(logger.Info)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: mysqlLogger,
+	})
+	if err != nil {
+		log.Fatalf(fmt.Sprintf("数据库连接失败: %s", dsn))
+	}
+	sqlDB, _ := db.DB()
+	sqlDB.SetMaxIdleConns(10)               // 设置连接池中的最大闲置连接数
+	sqlDB.SetMaxOpenConns(100)              // 设置连接池最大连接数
+	sqlDB.SetConnMaxLifetime(time.Hour * 4) // 设置连接池最大生存时间，不能超过mysql的wait_timeout
+	return db
+}
