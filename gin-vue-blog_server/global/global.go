96c07926f4969ed555f977d3d71d0c40091c44ae
AA12-G <811959019@qq.com>
2024-09-03 10:38:12 +0800

    添加了数据库和日志的初始化

--- gin-vue-blog_server/global/global.go
+++ gin-vue-blog_server/global/global.go
@@ -1,7 +1,13 @@
 package global
 
-import "gin-vue-blog_server/config"
+import (
+	"gin-vue-blog_server/config"
+	"github.com/sirupsen/logrus"
+	"gorm.io/gorm"
+)
 
 var (
 	Config *config.Config
+	DB     *gorm.DB
+	Log    *logrus.Logger
 )
