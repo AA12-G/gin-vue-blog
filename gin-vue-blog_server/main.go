52e65c30ebd375655260e9bba70550a9bc0aa997
AA12-G <811959019@qq.com>
2024-09-04 14:26:14 +0800

    程序的主入口

--- gin-vue-blog_server/main.go
+++ gin-vue-blog_server/main.go
@@ -3,11 +3,29 @@ package main
 import (
 	"fmt"
 	"gin-vue-blog_server/core"
+	"gin-vue-blog_server/flag"
 	"gin-vue-blog_server/global"
+	"gin-vue-blog_server/routers"
 )
 
 func main() {
 	//读取配置文件
 	core.InitConf()
-	fmt.Println(global.Config)
+	//初始化日志
+	global.Log = core.InitLogger()
+	//连接数据库
+	global.DB = core.InitGorm()
+
+	option := flag.Parse()
+	fmt.Println(option)
+	if flag.IsStopWeb(option) {
+		flag.SwitchOption(option)
+		return
+	}
+
+	router := routers.InitRouter()
+
+	addr := global.Config.System.Addr()
+	global.Log.Infof("gvb_server 运行在 %s", addr)
+	router.Run(addr) //端口默认8080
 }
