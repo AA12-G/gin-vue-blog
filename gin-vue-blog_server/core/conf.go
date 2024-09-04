6314cd3bfa6c2416a35462b44ad513614712c0b3
AA12-G <811959019@qq.com>
2024-09-04 14:20:41 +0800

    封装了一个新的错误码

--- gin-vue-blog_server/core/conf.go
+++ gin-vue-blog_server/core/conf.go
@@ -5,9 +5,12 @@ import (
 	"gin-vue-blog_server/config"
 	"gin-vue-blog_server/global"
 	"gopkg.in/yaml.v2"
+	"io/fs"
 	"io/ioutil"
 	"log"
 )
+
+const ConfigFile = "settings.yaml"
 
 // InitConf 读取yaml文件的配置
 func InitConf() {
@@ -23,4 +26,17 @@ func InitConf() {
 	}
 	log.Println("config yamlFile load Init success.")
 	global.Config = c
+}
+
+func SetYaml() error {
+	byteData, err := yaml.Marshal(global.Config)
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(ConfigFile, byteData, fs.ModePerm)
+	if err != nil {
+		return err
+	}
+	global.Log.Info("配置文件修改成功")
+	return nil
 }
--- gin-vue-blog_server/models/res/err_code.go
+++ gin-vue-blog_server/models/res/err_code.go
@@ -4,11 +4,13 @@ type ErrorCode int
 
 const (
 	SettingsError ErrorCode = 1001 //系统错误
+	AgrumentError ErrorCode = 1002 // 参数错误
 )
 
 // 错误状态码
 var (
 	ErrorMap = map[ErrorCode]string{
 		SettingsError: "系统错误",
+		AgrumentError: "参数错误",
 	}
 )
