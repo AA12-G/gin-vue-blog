4500c81044024ac23c584fcd3bd36ed89573dcbd
AA12-G <811959019@qq.com>
2024-09-07 16:58:48 +0800

    添加了一个优化查询图片的方法

---
+++ gin-vue-blog_server/api/images_api/image_name_list.go
@@ -0,0 +1,27 @@
+package images_api
+
+import (
+	"gin-vue-blog_server/global"
+	"gin-vue-blog_server/models"
+	"gin-vue-blog_server/models/res"
+	"github.com/gin-gonic/gin"
+)
+
+type ImageResponse struct {
+	ID   uint   `json:"id"`
+	Path string `json:"path"` // 图片路径
+	Name string `json:"name"` // 图片名称
+}
+
+// ImageNameListView 图片名称列表
+// @Tags 图片管理
+// @Summary 图片名称列表
+// @Description 图片名称列表
+// @Router /api/images_name [get]
+// @Produce json
+// @Success 200 {object} res.Response{data=[]ImageResponse}
+func (ImagesApi) ImageNameListView(c *gin.Context) {
+	var imageList []ImageResponse
+	global.DB.Model(models.BannerModel{}).Select("id", "path", "name").Scan(&imageList)
+	res.OkWithData(imageList, c)
+}
--- gin-vue-blog_server/routers/images_router.go
+++ gin-vue-blog_server/routers/images_router.go
@@ -4,8 +4,9 @@ import "gin-vue-blog_server/api"
 
 func (router RouterGroup) ImagesRouter() {
 	imagesApi := api.ApiGroupApp.ImagesApi
-	router.POST("/images", imagesApi.ImagesUploadView)  // POST 上传（添加）
-	router.GET("/images", imagesApi.ImageListView)      // GET 查询
-	router.DELETE("/images", imagesApi.ImageRemoveView) // DELETE 删除
-	router.PUT("/images", imagesApi.ImageUpdateView)    // PUT 修改
+	router.POST("/images", imagesApi.ImagesUploadView)      // POST 上传（添加）
+	router.GET("/images", imagesApi.ImageListView)          // GET 查询
+	router.GET("/images_name", imagesApi.ImageNameListView) // GET 精致查询
+	router.DELETE("/images", imagesApi.ImageRemoveView)     // DELETE 删除
+	router.PUT("/images", imagesApi.ImageUpdateView)        // PUT 修改
 }
