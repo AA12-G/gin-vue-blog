7d1ca040dd870e73b5dc6d8efc82745af7cc5038
AA12-G <811959019@qq.com>
2024-09-09 23:53:11 +0800

    中间件的使用

---
+++ gin-vue-blog_server/middleware/jwt_auth.go
@@ -0,0 +1,27 @@
+package middleware
+
+import (
+	"gin-vue-blog_server/models/res"
+	"gin-vue-blog_server/utils/jwts"
+	"github.com/gin-gonic/gin"
+)
+
+// 登录的中间件
+func JwtAuth() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		token := c.Request.Header.Get("token")
+		if token == "" {
+			res.FailWithMessage("未携带token", c)
+			c.Abort()
+			return
+		}
+		claims, err := jwts.ParseToken(token)
+		if err != nil {
+			res.FailWithMessage("token错误", c)
+			c.Abort()
+			return
+		}
+		// 登录的用户
+		c.Set("claims", claims)
+	}
+}
--- gin-vue-blog_server/routers/user_router.go
+++ gin-vue-blog_server/routers/user_router.go
@@ -1,8 +1,12 @@
 package routers
 
-import "gin-vue-blog_server/api"
+import (
+	"gin-vue-blog_server/api"
+	"gin-vue-blog_server/middleware"
+)
 
 func (router RouterGroup) UserRouter() {
 	userApi := api.ApiGroupApp.UserApi
-	router.POST("/user_email", userApi.EmailLoginView) // POST 上传（添加）
+	router.POST("/user_email", userApi.EmailLoginView)              // POST 上传（添加）
+	router.GET("/user", middleware.JwtAuth(), userApi.UserListView) // GET 查询
 }
