eb26bca361e3cd0219a323f0d14af9e21cbc9864
AA12-G <811959019@qq.com>
2024-09-11 09:27:52 +0800

    jwt的测试

--- gin-vue-blog_server/middleware/jwt_auth.go
+++ gin-vue-blog_server/middleware/jwt_auth.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"gin-vue-blog_server/models/ctype"
 	"gin-vue-blog_server/models/res"
 	"gin-vue-blog_server/utils/jwts"
 	"github.com/gin-gonic/gin"
@@ -22,6 +23,29 @@ func JwtAuth() gin.HandlerFunc {
 			return
 		}
 		// 登录的用户
+		c.Set("claims", claims)
+	}
+}
+func JwtAdmin() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		token := c.Request.Header.Get("token")
+		if token == "" {
+			res.FailWithMessage("未携带token", c)
+			c.Abort()
+			return
+		}
+		claims, err := jwts.ParseToken(token)
+		if err != nil {
+			res.FailWithMessage("token错误", c)
+			c.Abort()
+			return
+		}
+		// 登录的用户
+		if claims.Role != int(ctype.PermissionAdmin) {
+			res.FailWithMessage("权限错误", c)
+			c.Abort()
+			return
+		}
 		c.Set("claims", claims)
 	}
 }
--- gin-vue-blog_server/testdata/2.jwt测试.go
+++ gin-vue-blog_server/testdata/2.jwt测试.go
@@ -11,13 +11,13 @@ func main() {
 	core.InitConf()
 	global.Log = core.InitLogger()
 	token, err := jwts.GenToken(jwts.JwtPayLoad{
-		UserID:   1,
-		Role:     1,
-		Username: "zhangsan",
+		UserID: 1,
+		Role:   1,
+		//Username: "zhangsan",
 		NickName: "zs",
 	})
 	fmt.Println(token, err)
 
-	claims, err := jwts.ParseToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InpoYW5nc2FuIiwibmlja19uYW1lIjoienMiLCJyb2xlIjoxLCJ1c2VyX2lkIjoxLCJleHAiOjE3MjU4Nzk0MDMuNTk2NTcyOSwiaXNzIjoiMTIzNCJ9.wx6hlZyL0cVd1WxZ08NU_-PF_PO_KG5")
+	claims, err := jwts.ParseToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuaWNrX25hbWUiOiJ6cyIsInJvbGUiOjEsInVzZXJfaWQiOjEsImV4cCI6MTcyNTk0MjYxNC42MjgzMjQsImlzcyI6IjEyMzQifQ.KQrfxAgpRbbmsrh9QfDfStci3DcAMU0X-6IOG8O7aXY")
 	fmt.Println(claims, err)
 }
