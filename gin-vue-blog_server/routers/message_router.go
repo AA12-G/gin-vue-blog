8bdc9e005ee5b63ced33d0ab8408db147498f33b
AA12-G <811959019@qq.com>
2024-09-14 12:30:42 +0800

    查看聊天记录

--- gin-vue-blog_server/routers/message_router.go
+++ gin-vue-blog_server/routers/message_router.go
@@ -7,7 +7,8 @@ import (
 
 func (router RouterGroup) MessageRouter() {
 	messageApi := api.ApiGroupApp.MessageApi
-	router.POST("/message", middleware.JwtAuth(), messageApi.MessageCreateView) // POST 发送消息
-	router.GET("/message_all", messageApi.MessageListAllView)                   // 查看所有消息列表
-	router.GET("/message", middleware.JwtAuth(), messageApi.MessageListView)
+	router.POST("/message", middleware.JwtAuth(), messageApi.MessageCreateView)       // POST 发送消息
+	router.GET("/message_all", messageApi.MessageListAllView)                         // 查看所有消息列表(管理员)
+	router.GET("/message", middleware.JwtAuth(), messageApi.MessageListView)          // 用户的消息
+	router.GET("/message_record", middleware.JwtAuth(), messageApi.MessageRecordView) // 查看消息详情（聊天记录）
 }
